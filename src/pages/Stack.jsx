e1fa9db9d44b9c1d5b8be11aa6084afbf333f8ee
CristobalNPE <104582650+CristobalNPE@users.noreply.github.com>
2022-08-31 18:53:15 -0400

    add files for stack page

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import StackImg from '../assets/stack.png'
+import '../styles/Stack.css'
 
 const Stack = () => {
     return (
-        <div>
-            <h1>Stack</h1>
+        <div className="stack">
+
+            <div className="section-title">
+                <img className="section-title__img" src={StackImg} alt="Image of stack overflowing"/>
+                <h1 className="section-title__text">Stack</h1>
+                <hr className="section-title__line"/>
+            </div>
+
+
         </div>
     );
 };
